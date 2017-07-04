4c185944369d96ad7ebb452f3a17ad9bd734b64f
Sergey Shandar <sergey@live.com.au>
2017-07-04 11:47:45 -0700

    A lot of new functions.

--- Aliq/Extensions.cs
+++ Aliq/Extensions.cs
@@ -57,7 +57,7 @@ namespace Aliq
 
         public static Bag<Numeric<T, P>> NumericBag<T, P>(this Bag<T> input, P _ = default(P))
             where P : struct, INumericPolicy<T>
-            => input.Select(v => v.Numeric<T, P>());
+            => input.Select(v => v.Numeric(default(P)));
 
         public static Bag<Numeric<decimal, DecimalPolicy>> NumericBag(this Bag<decimal> input)
             => input.NumericBag(default(DecimalPolicy));
@@ -95,25 +95,9 @@ namespace Aliq
             where T : struct
             => input.SelectMany(v => v == null ? Enumerable.Empty<T>() : new[] { v.Value });
 
-        public static Bag<Numeric<T, P>> NumericBag<T, P>(this Bag<T?> input, P _ = default(P))
-            where P : struct, INumericPolicy<T>
-            where T : struct
-            => input.NotNullValues().NumericBag<T, P>();
-
-        public static Bag<Numeric<decimal, DecimalPolicy>> NumericBag(this Bag<decimal?> input)
-            => input.NumericBag(default(DecimalPolicy));
-
-        public static Bag<Numeric<double, DoublePolicy>> NumericBag(this Bag<double?> input)
-            => input.NumericBag(default(DoublePolicy));
-
-        public static Bag<Numeric<int, Int32Policy>> NumericBag(this Bag<int?> input)
-            => input.NumericBag(default(Int32Policy));
-
-        public static Bag<Numeric<long, Int64Policy>> NumericBag(this Bag<long?> input)
-            => input.NumericBag(default(Int64Policy));
-
-        public static Bag<Numeric<float, SinglePolicy>> NumericBag(this Bag<float?> input)
-            => input.NumericBag(default(SinglePolicy));
+        public static Bag<T> NotNullValues<T, I>(this Bag<I> input, Expression<Func<I, T?>> map)
+             where T : struct
+            => input.Select(map).NotNullValues();
 
         public static Bag<T?> NullableAverage<T, P>(this Bag<Numeric<T, P>> input)
             where P : struct, INumericPolicy<T>
@@ -138,18 +122,96 @@ namespace Aliq
             => input.NumericBag().Average();
 
         public static Bag<decimal?> Average(this Bag<decimal?> input)
-            => input.NumericBag().NullableAverage();
+            => input.NotNullValues().NumericBag().NullableAverage();
 
         public static Bag<double?> Average(this Bag<double?> input)
-            => input.NumericBag().NullableAverage();
+            => input.NotNullValues().NumericBag().NullableAverage();
 
         public static Bag<int?> Average(this Bag<int?> input)
-            => input.NumericBag().NullableAverage();
+            => input.NotNullValues().NumericBag().NullableAverage();
 
         public static Bag<long?> Average(this Bag<long?> input)
-            => input.NumericBag().NullableAverage();
+            => input.NotNullValues().NumericBag().NullableAverage();
 
         public static Bag<float?> Average(this Bag<float?> input)
-            => input.NumericBag().NullableAverage();
+            => input.NotNullValues().NumericBag().NullableAverage();
+
+        public static Bag<decimal> Average<T>(
+            this Bag<T> input, Expression<Func<T, decimal>> map)
+            => input.Select(map).NumericBag().Average();
+
+        public static Bag<double> Average<T>(
+            this Bag<T> input, Expression<Func<T, double>> map)
+            => input.Select(map).NumericBag().Average();
+
+        public static Bag<int> Average<T>(
+            this Bag<T> input, Expression<Func<T, int>> map)
+            => input.Select(map).NumericBag().Average();
+
+        public static Bag<long> Average<T>(
+            this Bag<T> input, Expression<Func<T, long>> map)
+            => input.Select(map).NumericBag().Average();
+
+        public static Bag<float> Average<T>(
+            this Bag<T> input, Expression<Func<T, float>> map)
+            => input.Select(map).NumericBag().Average();
+
+        public static Bag<decimal?> Average<T>(
+            this Bag<T> input, Expression<Func<T, decimal?>> map)
+            => input.NotNullValues(map).NumericBag().NullableAverage();
+
+        public static Bag<double?> Average<T>(
+            this Bag<T> input, Expression<Func<T, double?>> map)
+            => input.NotNullValues(map).NumericBag().NullableAverage();
+
+        public static Bag<int?> Average<T>(
+            this Bag<T> input, Expression<Func<T, int?>> map)
+            => input.NotNullValues(map).NumericBag().NullableAverage();
+
+        public static Bag<long?> Average<T>(
+            this Bag<T> input, Expression<Func<T, long?>> map)
+            => input.NotNullValues(map).NumericBag().NullableAverage();
+
+        public static Bag<float?> Average<T>(
+            this Bag<T> input, Expression<Func<T, float?>> map)
+            => input.NotNullValues(map).NumericBag().NullableAverage();
+
+        public static Bag<bool> Contains<T>(Bag<T> input, T value)
+            => input.Any(v => v.Equals(value));
+
+        public static Bag<T> Sum<T, P>(this Bag<Numeric<T, P>> input)
+            where P : struct, INumericPolicy<T>
+            => input
+                .Aggregate(0L.ToNumeric<T, P>(), (a, b) => a.Add(b))
+                .Select(v => v.Value);
+
+        public static Bag<long> Sum(this Bag<long> input)
+            => input.NumericBag(default(Int64Policy)).Sum();
+
+        public static Bag<long> Count<T>(this Bag<T> input)
+            => input.Select(_ => 1L).Sum();
+
+        public static Bag<long> Count<T>(
+            this Bag<T> input, Expression<Func<T, bool>> p)
+            => input.Where(p).Count();
+
+        public static Bag<T> DefaultIfEmpty<T>(this Bag<T> input, T value = default(T))
+            => input
+                .Any()
+                .Where(v => !v)
+                .Select(_ => value)
+                .DisjointUnion(input);
+
+        public static NumberOf<T> NumberOf<T>(this T value, long count)
+            => new NumberOf<T>(value, count);
+
+        public static NumberOf<T> NumberOf<T>(this T value)
+            => value.NumberOf(1);
+
+        public static Bag<T> Distinct<T>(this Bag<T> input)
+            => input
+                .Select(v => v.NumberOf())
+                .GroupBy(v => v.Value, (a, b) => a.Add(b.Count))
+                .Select(v => v.Value);
     }
 }
