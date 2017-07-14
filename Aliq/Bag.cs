60e2642c426cbe37c8c922afa3d4374b485e1a4b
Deepak Shankargouda <deshank@microsoft.com>
2017-07-14 16:55:58 -0700

    Update Bag.cs

--- Aliq/Bag.cs
+++ Aliq/Bag.cs
@@ -1,15 +1,29 @@
 ﻿namespace Aliq
 {
+    // Bag is a multiset (unordered list of elements which may contain duplicates)
     public abstract class Bag
     {
         public interface IVisitor<R>
         {
+            
             R Visit<T>(Bag<T> bag);
         }
 
+        // Example:
+        // Bag b;
+        // class Visitor : Bag.IVisitor<string>
+        // {
+        //    public string Visit<T>(Bag<T> bag)
+        //    {
+        //        return typeof(T).Name;
+        //    }
+        // }
+        //
+        // string itemTypeName = b.Accept(new Visitor());
         public abstract R Accept<R>(IVisitor<R> visitor);
     }
 
+    // A Bag which contains elements of type T
     public abstract class Bag<T> : Bag
     {
         public sealed override R Accept<R>(Bag.IVisitor<R> visitor)
