e88dbbd86d05d5fb6097d1bba1e6295073d44a48
Sergey Shandar <sergey@live.com.au>
2017-07-24 15:48:30 -0700

    Assembly Loader

---
+++ RemoteBackEnd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Loader;
+
+namespace RemoteBackEnd
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("no arguments");
+                return -1;
+            }
+            var dllName = args[0];
+            var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(dllName);
+            return 0;
+        }
+    }
+}
