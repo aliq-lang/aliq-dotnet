2836402effc2c115368ef66be8d7839fcd1e1578
mikeliu8492 <mikeliu2@illinois.edu>
2017-07-24 14:31:38 -0800

    new unit tests

--- XUnitTest/InMemoryTest.cs
+++ XUnitTest/InMemoryTest.cs
@@ -1,4 +1,5 @@
 using Aliq;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -6,6 +7,7 @@ namespace XUnitTest
 {
     public class InMemoryTest
     {
+
         [Fact]
         public void TestSetInput()
         {
@@ -46,7 +48,80 @@ namespace XUnitTest
             var gNew = inMemory.Get(g);
             Assert.Equal(1, gNew.Count());
             var item = gNew.First();
-            Assert.Equal("Hellpworld!".Length, item.Length);
+            Assert.Equal("Helloworld!".Length, item.Length);
         }
+
+        [Fact]
+        public void TestGroupByIntegersSum()
+        {
+            // logic
+            var a = new ExternalInput<int>();
+            var g = a.Aggregate((ai, bi) => ai + bi);
+
+            // data
+            var aTable = new[] { 8, 30, 4};
+
+            // back end
+            var inMemory = new InMemory();
+
+            // binding
+            inMemory.SetInput(a, aTable);
+
+            // get
+            var gNew = inMemory.Get(g);
+            Assert.Equal(1, gNew.Count());
+            var item = gNew.First();
+            Assert.Equal(42, item);
+        }
+
+        [Fact]
+        public void TestGroupByDoubleMean()
+        {
+            // logic
+            var a = new ExternalInput<double>();
+            var g = a.Average();
+
+            // data
+            double[] aTable = (new[] { 8.0, 30.0, 4.0, 18.0 });
+            var avgWithLINQ = aTable.Average();
+            Assert.Equal(15, avgWithLINQ);
+
+            // back end
+            var inMemory = new InMemory();
+
+            // binding
+            inMemory.SetInput<double>(a, aTable);
+
+            // get
+            var gNew = inMemory.Get(g);
+            Assert.Equal(1, gNew.Count());
+            var item = gNew.First();
+            Assert.Equal(14, item);
+        }
+
+        [Fact]
+        public void MapReduceTest()
+        {
+
+
+            int[] numbers = { 10, 15, 20, 25, 30, 35 };
+            //List<int>
+
+            foreach (int number in numbers)
+            {
+                int curNumber = number + 3;
+            }
+
+            // logic
+            var a = new ExternalInput<int>();
+            var g = a.Aggregate((ai, bi) => ai + bi);
+
+
+
+            var result = numbers.GroupBy(n => (n % 10 == 0));
+            var max = result.ElementAt(0);
+            Assert.Equal(6, numbers.Length);
+        }
+
     }
 }
