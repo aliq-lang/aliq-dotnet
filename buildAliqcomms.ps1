b1b490bcbdfe071ee5cade98f14f7f8ed8e3f8a3
dsgouda <deshank@microsoft.com>
2017-07-27 05:50:43 +0000

    Appease appveyor dont build server/worker

--- buildAliqcomms.ps1
+++
@@ -1,5 +0,0 @@
-dotnet msbuild AliqComms/AliqWorker/AliqWorker/AliqWorker.csproj /t:restore
-dotnet msbuild AliqComms/AliqWorker/AliqWorker/AliqWorker.csproj
-
-dotnet msbuild AliqComms/AliqServer/AliqServer/AliqServer.csproj /t:restore
-dotnet msbuild AliqComms/AliqServer/AliqServer/AliqServer.csproj
