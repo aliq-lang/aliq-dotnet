2496328b1042b8a0158493a056ed957288b360f9
Sergey Shandar <sergey@live.com.au>
2017-08-27 05:14:29 -0700

    shared functions

--- Aliq/Adapters/HotObservableAdapter.cs
+++ Aliq/Adapters/HotObservableAdapter.cs
@@ -31,35 +31,11 @@ namespace Aliq.Adapters
             {
             }
 
-            public override IObservable<T> Visit<I>(SelectMany<T, I> selectMany)
-                => Adapter.Get(selectMany.Input).SelectMany(selectMany.Func);
-
-            public override IObservable<T> Visit(Merge<T> merge)
-            {
-                var a = Adapter.Get(merge.InputA);
-                var b = Adapter.Get(merge.InputB);
-                return a.Merge(b);
-            }
-
             public override IObservable<T> Visit(ExternalInput<T> externalInput)
                 => new Subject<T>();
 
             public override IObservable<T> Visit(Const<T> const_)
                 => Adapter.StartSubject.Select(_ => const_.Value);
-
-            public override IObservable<T> Visit<I>(GroupBy<T, I> groupBy)
-                => Adapter
-                    .Get(groupBy.Input)
-                    .GroupBy(x => x.Key, x => x.Value)
-                    .SelectMany(g => g.Aggregate(groupBy.Reduce).Select(v => (g.Key, v)))
-                    .SelectMany(groupBy.GetResult);
-
-            public override IObservable<T> Visit<A, B>(Product<T, A, B> product)
-            {
-                var a = Adapter.Get(product.InputA);
-                var b = Adapter.Get(product.InputB);
-                return a.SelectMany(ai => b.SelectMany(bi => product.Func(ai, bi)));
-            }
         }
     }
 }
--- Aliq/Adapters/ObservableAdapterBase.cs
+++ Aliq/Adapters/ObservableAdapterBase.cs
@@ -1,9 +1,11 @@
 ﻿using Aliq.Bags;
 using System;
+using System.Reactive.Linq;
 
 namespace Aliq.Adapters
 {
     public abstract class ObservableAdapterBase<A>
+        where A : ObservableAdapterBase<A>
     {
         public IObservable<T> Get<T>(Bag<T> bag)
             => Map.GetOrCreate(bag, () => bag.Accept(CreateCreateVisitor<T>()));
@@ -19,12 +21,32 @@ namespace Aliq.Adapters
                 Adapter = adapter;
             }
 
-            public abstract IObservable<T> Visit<I>(SelectMany<T, I> selectMany);
-            public abstract IObservable<T> Visit(Merge<T> merge);
+            public IObservable<T> Visit<I>(SelectMany<T, I> selectMany)
+                => Adapter.Get(selectMany.Input).SelectMany(selectMany.Func);
+
+            public IObservable<T> Visit(Merge<T> merge)
+            {
+                var a = Adapter.Get(merge.InputA);
+                var b = Adapter.Get(merge.InputB);
+                return a.Merge(b);
+            }
+
             public abstract IObservable<T> Visit(ExternalInput<T> externalInput);
             public abstract IObservable<T> Visit(Const<T> const_);
-            public abstract IObservable<T> Visit<I>(GroupBy<T, I> groupBy);
-            public abstract IObservable<T> Visit<A, B>(Product<T, A, B> product);
+
+            public IObservable<T> Visit<I>(GroupBy<T, I> groupBy)
+                => Adapter
+                    .Get(groupBy.Input)
+                    .GroupBy(x => x.Key, x => x.Value)
+                    .SelectMany(g => g.Aggregate(groupBy.Reduce).Select(v => (g.Key, v)))
+                    .SelectMany(groupBy.GetResult);
+
+            public IObservable<T> Visit<A, B>(Product<T, A, B> product)
+            {
+                var a = Adapter.Get(product.InputA);
+                var b = Adapter.Get(product.InputB);
+                return a.SelectMany(ai => b.SelectMany(bi => product.Func(ai, bi)));
+            }
 
             protected A Adapter { get; }
         }
